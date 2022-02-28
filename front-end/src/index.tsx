6f4591ead8f93e038cb90755af0b3012ebe9509b
Artur Wagner <35738771+ArturWagnerBusiness@users.noreply.github.com>
2022-02-28 21:13:35 +0000

    Persistent theme.

--- front-end/src/index.tsx
+++ front-end/src/index.tsx
@@ -3,14 +3,26 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
-
+import Cookies from "universal-cookie";
+const cookies = new Cookies();
 const darkTheme = createTheme({ palette: { mode: "dark" } });
 const lightTheme = createTheme({ palette: { mode: "light" } });
 
 export default class Index extends Component<{}, { theme: boolean }> {
-  state = {
-    theme: false,
-  };
+  constructor(props: any) {
+    super(props);
+    let theme = cookies.get("theme");
+    if (theme === undefined) {
+      cookies.set("theme", false, { path: "/" });
+      this.state = {
+        theme: false,
+      };
+    } else {
+      this.state = {
+        theme,
+      };
+    }
+  }
   render() {
     return (
       <ThemeProvider theme={this.state.theme ? darkTheme : lightTheme}>
@@ -18,6 +30,7 @@ export default class Index extends Component<{}, { theme: boolean }> {
         <App
           theme={this.state.theme}
           updateTheme={(event) => {
+            cookies.set("theme", event.target.checked, { path: "/" });
             this.setState({ theme: event.target.checked });
           }}
         />
